04d8fbe6574de7de6e29681672830aaeb87f26d4
Dawid Osiński <dawidosinski59@gmail.com>
2023-09-12 16:01:09 +0200

    animations fade-in fade-out added

--- app/js/script.js
+++ app/js/script.js
@@ -1,5 +1,16 @@
 const btnHamburger = document.querySelector("#btnHamburger");
+const header = document.querySelector(".header");
+const overlay = document.querySelector(".overlay");
 
-btnHamburger.addEventListener("click", () => {
-    btnHamburger.classList.toggle("open");
-})
+btnHamburger.addEventListener("click", function () {
+
+  if (header.classList.contains("open")) {    //Close Ham Menu
+    header.classList.remove("open");
+    overlay.classList.remove("fade-in");
+    overlay.classList.add("fade-out");
+  } else {                                    //Open Ham Menu
+    header.classList.add("open");
+    overlay.classList.remove("fade-out");
+    overlay.classList.add("fade-in");
+  }
+});
